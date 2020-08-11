f7c9920c7bc6638489dc3a8844f039a0aa1df565
Andre Barranco <b.andrefilipe@gmail.com>
2020-08-11 11:52:20 -0300

    #11 Remover anotacao @EnableWebMvc

--- adapters/brewer-web/src/main/java/io/bandrefilipe/brewer/web/config/web/WebConfig.java
+++ adapters/brewer-web/src/main/java/io/bandrefilipe/brewer/web/config/web/WebConfig.java
@@ -22,7 +22,6 @@
 package io.bandrefilipe.brewer.web.config.web;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,7 +29,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author bandrefilipe
  * @since 1.0.0
  */
-@EnableWebMvc
 @Configuration
 class WebConfig implements WebMvcConfigurer {
 
