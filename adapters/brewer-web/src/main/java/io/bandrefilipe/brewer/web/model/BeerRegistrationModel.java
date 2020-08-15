921fec18e1cecea9ac51db3e62af9ed008eeede5
André Barranco <39977213+bandrefilipe@users.noreply.github.com>
2020-08-15 17:42:26 -0300

    #14 Capitulo 4 - O Thymeleaf

--- adapters/brewer-web/src/main/java/io/bandrefilipe/brewer/web/model/BeerRegistrationModel.java
+++ adapters/brewer-web/src/main/java/io/bandrefilipe/brewer/web/model/BeerRegistrationModel.java
@@ -1,7 +1,9 @@
 package io.bandrefilipe.brewer.web.model;
 
+import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -10,11 +12,20 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 public final class BeerRegistrationModel {
 
-    @NotBlank
+    private static final int MAX_DESCRIPTION_LENGTH = 140;
+
+    @NotBlank(message = "sku is required")
     private String sku;
 
+    @NotBlank(message = "name is required")
     private String name;
 
-    @Size(max = 140)
+    @Size(
+        max = MAX_DESCRIPTION_LENGTH,
+        message = "description must have up to " + MAX_DESCRIPTION_LENGTH + " characters"
+    )
     private String description;
+
+    @Setter(AccessLevel.PRIVATE)
+    private int descriptionMaxLength = MAX_DESCRIPTION_LENGTH;
 }
