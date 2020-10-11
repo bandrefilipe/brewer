4f6de7a0351a7b9b9111b78b519a7d27375b709f
Andre Barranco <b.andrefilipe@gmail.com>
2020-10-10 23:42:00 -0300

    #21 Implementa classe BeerEntity

--- adapters/brewer-persistence/README.md
+++ adapters/brewer-persistence/README.md
@@ -1,2 +1,8 @@
 # Adapter: Brewer Persistence
 (work pending)
+
+## Companion Articles
+- [The Power of a Good SQL Naming Convention](https://www.xaprb.com/blog/2008/10/26/the-power-of-a-good-sql-naming-convention/ "xaprb.com")
+- [How to implement Equals and HashCode for JPA entities](https://vladmihalcea.com/hibernate-facts-equals-and-hashcode/ "vladmihalcea.com")
+- [How to implement equals and hashCode using the JPA entity identifier (Primary Key)](https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/ "vladmihalcea.com")
+- [The best way to map a @NaturalId business key with JPA and Hibernate](https://vladmihalcea.com/the-best-way-to-map-a-naturalid-business-key-with-jpa-and-hibernate/ "vladmihalcea.com")
--- adapters/brewer-persistence/src/main/java/io/bandrefilipe/brewer/persistence/model/BeerEntity.java
+++ adapters/brewer-persistence/src/main/java/io/bandrefilipe/brewer/persistence/model/BeerEntity.java
@@ -21,12 +21,21 @@
  */
 package io.bandrefilipe.brewer.persistence.model;
 
+import io.bandrefilipe.brewer.commons.identifier.Identifiable;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Objects;
 
 /**
  * @author bandrefilipe
@@ -34,11 +43,63 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "beer")
-class BeerEntity {
+@NoArgsConstructor @Getter @Setter
+class BeerEntity implements Identifiable<Long> {
+
+    @Id
+    @Column(name = "beer")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /** The Business Key */
+    @NaturalId
+    @Column(name = "sku", unique = true, nullable = false, updatable = false)
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    /** Max possible value: 999999.99 */
+    @Column(name = "unit_price", precision = 8, scale = 2)
+    private BigDecimal unitPrice;
+
+    /** Max possible value: 999.99 | Max expected value: 100.00 */
+    @Column(name = "alcohol_content", precision = 5, scale = 2)
+    private BigDecimal alcoholContent;
+
+    /** Max possible value: 999.99 | Max expected value: 100.00 */
+    @Column(name = "commission", precision = 5, scale = 2)
+    private BigDecimal commission;
+
+    @Column(name = "stock")
+    private Integer stock;
 
     @Column(name = "origin", length = 1)
     @Convert(converter = OriginConverter.class)
     private Origin origin;
+
+    public String getSimpleNaturalId() {
+        return this.getSku();
+    }
+
+    public int hashCode() {
+        return Objects.hashCode(sku);
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (other == null || this.getClass() != other.getClass()) {
+            return false;
+        }
+        BeerEntity that = (BeerEntity) other;
+        return Objects.equals(this.sku, that.sku);
+    }
 
     @Getter
     enum Origin {
